eebaad94b46ee22b0f14dd055326b56bfb28fc9c
donetian-petkov <donetian+petkov@gmail.com>
2022-08-07 11:54:54 +0300

    Initial Form Validation

--- src/components/authComponents/LoginComponent.js
+++ src/components/authComponents/LoginComponent.js
@@ -3,18 +3,39 @@ import './LoginComponent.css';
 
 export const LoginComponent = (props) => {
 
-    let [authMode, setAuthMode] = useState("signin")
+    let [authMode, setAuthMode] = useState("signin");
+
+    const [values, setValues] = useState({
+        username: '',
+        email: '',
+        password: '',
+    });
+
+    const submitHandler = (e) => {
+
+        e.preventDefault();
+
+    };
+
+    const changeHandler = (e) => {
+        setValues(state => ({
+            ...state,
+            [e.target.name]: e.target.type === 'checkbox' ? e.target.checked : e.target.value
+        }));
+    };
+
+
 
     const changeAuthMode = () => {
-        setAuthMode(authMode === "signin" ? "signup" : "signin")
-    }
+        setAuthMode(authMode === "signin" ? "signup" : "signin");
+    };
 
     if (authMode === "signin") {
         return (
             <div className="Auth-form-container">
-                <form className="Auth-form">
+                <form className="Auth-form" onSubmit={submitHandler}>
                     <div className="Auth-form-content">
-                        <h3 className="Auth-form-title">Sign In</h3>
+                        <h3 className="Auth-form-title">Login</h3>
                         <div className="text-center">
                             Not registered yet?{" "}
                             <span className="link-primary" onClick={changeAuthMode}>
@@ -27,6 +48,9 @@ export const LoginComponent = (props) => {
                                 type="email"
                                 className="form-control mt-1"
                                 placeholder="Enter email"
+                                name='email'
+                                value={values.email}
+                                onChange={changeHandler}
                             />
                         </div>
                         <div className="form-group mt-3">
@@ -35,6 +59,9 @@ export const LoginComponent = (props) => {
                                 type="password"
                                 className="form-control mt-1"
                                 placeholder="Enter password"
+                                name='password'
+                                value={values.password}
+                                onChange={changeHandler}
                             />
                         </div>
                         <div className="d-grid gap-2 mt-3">
@@ -42,9 +69,6 @@ export const LoginComponent = (props) => {
                                 Submit
                             </button>
                         </div>
-                        <p className="text-center mt-2">
-                            Forgot <a href="#">password?</a>
-                        </p>
                     </div>
                 </form>
             </div>
@@ -55,7 +79,7 @@ export const LoginComponent = (props) => {
         <div className="Auth-form-container">
             <form className="Auth-form">
                 <div className="Auth-form-content">
-                    <h3 className="Auth-form-title">Sign In</h3>
+                    <h3 className="Auth-form-title">Register</h3>
                     <div className="text-center">
                         Already registered?{" "}
                         <span className="link-primary" onClick={changeAuthMode}>
@@ -91,9 +115,6 @@ export const LoginComponent = (props) => {
                             Submit
                         </button>
                     </div>
-                    <p className="text-center mt-2">
-                        Forgot <a href="#">password?</a>
-                    </p>
                 </div>
             </form>
         </div>
