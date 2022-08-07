6886fa829b6b34c869909313adb412975765146d
donetian-petkov <donetian+petkov@gmail.com>
2022-08-07 13:51:04 +0300

    Initial AuthService + Requester

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,10 @@
+import {Link} from "react-router-dom";
+
 export const Footer = () => {
 
     return (
         <div id="footer">
-            <p className="lf">Copyright &copy; 2022 <a href="#">Movie Hunter</a> - All Rights Reserved</p>
+            <p className="lf">Copyright &copy; 2022 <Link to="/">Movie Hunter</Link> - All Rights Reserved</p>
             <div style={{clear: 'both'}}/>
         </div>
     )
--- src/components/authComponents/LoginComponent.js
+++ src/components/authComponents/LoginComponent.js
@@ -15,12 +15,18 @@ export const LoginComponent = (props) => {
 
         e.preventDefault();
 
+        if (authMode === 'signin') {
+
+        } else if (authMode === 'signup') {
+
+        }
+
     };
 
     const changeHandler = (e) => {
         setValues(state => ({
             ...state,
-            [e.target.name]: e.target.type === 'checkbox' ? e.target.checked : e.target.value
+            [e.target.name]: e.target.value
         }));
     };
 
@@ -77,7 +83,7 @@ export const LoginComponent = (props) => {
 
     return (
         <div className="Auth-form-container">
-            <form className="Auth-form">
+            <form className="Auth-form" onSubmit={submitHandler}>
                 <div className="Auth-form-content">
                     <h3 className="Auth-form-title">Register</h3>
                     <div className="text-center">
@@ -87,11 +93,14 @@ export const LoginComponent = (props) => {
             </span>
                     </div>
                     <div className="form-group mt-3">
-                        <label>Full Name</label>
+                        <label>Username</label>
                         <input
-                            type="email"
+                            type="text"
                             className="form-control mt-1"
                             placeholder="e.g Jane Doe"
+                            name='username'
+                            value={values.username}
+                            onChange={changeHandler}
                         />
                     </div>
                     <div className="form-group mt-3">
@@ -100,6 +109,9 @@ export const LoginComponent = (props) => {
                             type="email"
                             className="form-control mt-1"
                             placeholder="Email Address"
+                            name='email'
+                            value={values.email}
+                            onChange={changeHandler}
                         />
                     </div>
                     <div className="form-group mt-3">
@@ -108,6 +120,9 @@ export const LoginComponent = (props) => {
                             type="password"
                             className="form-control mt-1"
                             placeholder="Password"
+                            name='password'
+                            value={values.password}
+                            onChange={changeHandler}
                         />
                     </div>
                     <div className="d-grid gap-2 mt-3">
