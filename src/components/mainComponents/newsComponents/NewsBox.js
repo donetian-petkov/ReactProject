29e7d6234bfc0b1ad3340dcd6f90b9e28d82bc0f
donetian-petkov <donetian+petkov@gmail.com>
2023-02-14 23:45:50 +0200

    Change of API

--- src/components/mainComponents/newsComponents/NewsBox.js
+++ src/components/mainComponents/newsComponents/NewsBox.js
@@ -4,7 +4,7 @@ export const NewsBox = (props) => {
         <div className="content">
             <p className="date">{props.news?.published_date}</p>
             <h5 style={{"color": "#faaf00"}}>{props.news?.title}</h5>
-            <p>&quot;{props.news?.excerpt}&quot;</p>
+            {/*<p>&quot;{props.news?.description}&quot;</p>*/}
             <a href={props.news?.url} target="_blank">Read more</a>
         </div>
     )
